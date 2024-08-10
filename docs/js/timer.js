dc5660d898219b6f9feb63f3019cedb7b6ff5c0f
o.gurov <o.gurov@itransition.com>
2024-08-10 13:43:16 +0500

    Click instead of double click

--- docs/js/timer.js
+++ docs/js/timer.js
@@ -336,7 +336,7 @@ function createCardElement(date, title, value) {
     </div>  
     <div class="card-content">
 	  	<div class="date">${date}</div>
-    	<div id="cardTime" class="time" title="Double-click to edit">
+    	<div id="cardTime" class="time" title="Click to edit">
     		<span id="editableTimer">${value}</span>
 
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3"
@@ -346,7 +346,7 @@ function createCardElement(date, title, value) {
           <path d="M13.5 6.5l4 4" />
         </svg>
     </div>
-    	<div id="cardTitle" class="description" title="Double-click to edit">${title}</div>
+    	<div id="cardTitle" class="description" title="Click to edit">${title}</div>
     </div>`;
 
   card.insertAdjacentHTML("beforeEnd", rest);
@@ -385,7 +385,7 @@ function createCardElement(date, title, value) {
   // time edit handler
   const timeBlock = card.querySelector("#cardTime");
 
-  timeBlock.addEventListener("dblclick", function (e) {
+  timeBlock.addEventListener("click", function (e) {
     e.stopPropagation();
 
     state.editedvalue = value; // add current value to state to return it if input is invalid
@@ -407,7 +407,7 @@ function createCardElement(date, title, value) {
   // title edit handler
   const cardTitle = card.querySelector("#cardTitle");
 
-  cardTitle.addEventListener("dblclick", function (e) {
+  cardTitle.addEventListener("click", function (e) {
     e.stopPropagation();
 
     this.contentEditable = true;
